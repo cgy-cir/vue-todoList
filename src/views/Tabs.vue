facf58e454b087939360f8a3daaea97fbd9a1026
cgy <593056561@qq.com>
2020-04-22 22:24:41 +0800

    demo

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="tabs" >
-    <span class="unfinish-item">未完成数据{{unfinishItem}}</span>
-    <span 
-      v-for="(state,index) in states" 
+  <div class="tabs">
+    <span class="unfinish-item">未完成数据: {{unfinishItem}}</span>
+    <span
+      v-for="(state,index) in states"
       :key="index"
-      @click="$emit('toggleFilter',state)"
-      :class="active === state ? '未完成' : ''"
-      
-    >
-      {{state}}
-    </span>
-    <span 
-      class="clear-finish"
-      @click="$emit('clearAllCompleted')"
-      v-show="show"
-    >
-      清除已完成
-    </span>
+      @click="$emit('toggleFilter',state);changeActive(state)"
+      class="abc"
+    >{{state}}</span>
+    <span class="clear-finish" @click="$emit('clearAllCompleted')" v-show="show">清除已完成</span>
   </div>
 </template>
 
 
 <script>
-
 export default {
   props: {
     todos: {
@@ -35,35 +25,61 @@ export default {
   },
   data() {
     return {
-      states: ['全部','未完成','已完成']
-    }
+      states: ["全部", "未完成", "已完成"]
+    };
+  },
+  mounted() {
+    let all = document.getElementsByClassName("abc")[0];
+    all.classList.add("active");
   },
   computed: {
     unfinishItem() {
-      return this.todos.filter(todo => !todo.state).length
+      return this.todos.filter(todo => !todo.state).length;
     },
     show() {
-      return this.todos.filter(todo => todo.state).length !== 0
+      return this.todos.filter(todo => todo.state).length !== 0;
     }
   },
-}
+  methods: {
+    changeActive(state) {
+      let all = document.getElementsByClassName("abc")[0];
+      let unfinish = document.getElementsByClassName("abc")[1];
+      let finished = document.getElementsByClassName("abc")[2];
+      all.className = "";
+      unfinish.className = "";
+      finished.className = "";
+      if (state == "全部") {
+        all.classList.add("active");
+      } else if (state == "未完成") {
+        unfinish.classList.add("active");
+      } else if (state == "已完成") {
+        finished.classList.add("active");
+      }
+      all.classList.add("abc");
+      unfinish.classList.add("abc");
+      finished.classList.add("abc");
+    }
+  }
+};
 </script>
 
 
 <style scoped>
-.tabs{
+.tabs {
   width: 600px;
   margin: 20px auto;
 }
 
-.active{
+.active {
   border: 1px solid black;
-
 }
-span{
-  margin: 0 20px;
+span {
+  cursor: pointer;
+  margin: 20px;
+  padding: 5px;
+  border-radius: 5px;
 }
-.clear-finish{
+.clear-finish {
   margin-left: 50px;
 }
 </style>
--- src/views/TodoItem.vue
+++ src/views/TodoItem.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="todo-item">
-    <input class="todo-checkbox" type="checkbox" v-model="todo.state">
-    <span class="todo-title"
+    <input class="todo-checkbox" type="checkbox" v-model="todo.state" />
+    <span
+      class="todo-title"
       :class="{active:todo.state}"
       v-if="!todo.isEdit"
       v-text="todo.title"
       @click="todo.isEdit = !todo.isEdit"
-
-    >
-    </span>
+    ></span>
     <!-- 可修改 -->
-    <input 
+    <input
       :class="{active:todo.state}"
       class="edit-title"
       v-else
-      type="text" 
-      v-model="todo.title" 
+      type="text"
+      v-model="todo.title"
       @blur="todo.isEdit = !todo.isEdit"
       @keyup.enter="todo.isEdit = !todo.isEdit"
       :disabled="todo.state"
-    >
+    />
 
     <button class="remove-title" v-show="todo.state" @click="$emit('remove')">删除</button>
   </div>
@@ -30,57 +29,53 @@ export default {
   props: {
     todo: {
       type: Object,
-      required:true
+      required: true
     }
   },
   data() {
-    return {
-      
-    }
-  },
-
-}
+    return {};
+  }
+};
 </script>
 
 <style scoped>
-  .todo-item{
-    position: relative;
-    left: 72px;
-    width: 600px;
-    margin: 0 auto;
-  }
-  .active{
-    text-decoration:line-through;
-    background-color:white;
-    color: gainsboro;
-  }
-  .todo-title{
-    text-align: left;
-    line-height: 30px;
-    display: inline-block;
-    /* border-bottom: 1px solid black;
+.todo-item {
+  position: relative;
+  left: 72px;
+  width: 600px;
+  margin: 0 auto;
+}
+.active {
+  text-decoration: line-through;
+  background-color: white;
+  color: gainsboro;
+}
+.todo-title {
+  text-align: left;
+  line-height: 30px;
+  display: inline-block;
+  /* border-bottom: 1px solid black;
     border-top: 1px solid transparent; */
-    width: 300px;
-    height: 30px;
-    padding-left: 15px;
-    margin-top: 10px
-  }
-  .edit-title{
-    width: 300px;
-    height: 30px;
-    padding: 0 0 0 15px;
-    border-style:none;
-    margin-top: 10px;
-    /* border-bottom: 1px solid black;
+  width: 300px;
+  height: 30px;
+  padding-left: 15px;
+  margin-top: 10px;
+}
+.edit-title {
+  width: 300px;
+  height: 30px;
+  padding: 0 0 0 15px;
+  border-style: none;
+  margin-top: 10px;
+  /* border-bottom: 1px solid black;
     border-top: 1px solid transparent; */
-  }
-  .todo-checkbox{
-    position: absolute;
-    top: 19px;
-    left: -60px;
-  }
-  .remove-title{
+}
+.todo-checkbox {
+  position: absolute;
+  top: 19px;
+  left: -60px;
+}
+.remove-title {
   margin-left: 26px;
-  }
-
+}
 </style>
